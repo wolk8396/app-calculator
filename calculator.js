5b267f317d153ce38a79fcb16459d917f6a6b154
Sergey_Petrachev <mastertso2241@gmail.com>
2024-03-24 01:49:00 +0300

    change logic

--- calculator.js
+++ calculator.js
@@ -4,40 +4,31 @@ const result = document.querySelector('.results');
 const displayResult = document.querySelector('.operations');
 const numbers = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const mathematicalOperations = ['/', '*', '-', '+', '%'];
+const calculatorRegex = /^-?\d+(\.\d+)?\s*[-+*/%]\s*\d+(\.\d+)?$/;
+const replaceStr = /(-?\d+)\s*([-+*/])\s*/g;
+const regex = /-?\d+|[\/\*\-\+%]/g;
 const error  = 'деление на ноль невозможно';
+let finalResults = false;
 let firstValue ='';
 let secondValue = '';
 let signMathematicalOperations ='';
 let endOperation = false;
+let date = '0';
+let str = '';
 
 const onReset = () => {
   endOperation = false;
+  finalResults = false;
   firstValue = '';
   secondValue = '';
   signMathematicalOperations = '';
   result.innerHTML = '0';
   displayResult.innerHTML = '';
+  date = '0';
+  str = '';
 };
 
 bet_reset.addEventListener('click', onReset);
-
-const onSecondNumber = num => {
-  if (endOperation) {
-    secondValue = '';
-    endOperation = false;
-  };
-  secondValue += num;
-  result.innerHTML = secondValue;
-};
-
-const onOperations = value => {
-  endOperation = false;
-  signMathematicalOperations = value;
-  if (firstValue === '') {
-    firstValue = secondValue;
-    secondValue = '';
-  }
-};
 
 const onRenderOperation = () => {
   const results = `${firstValue} ${signMathematicalOperations} ${secondValue}`;
@@ -49,7 +40,7 @@ const calculateResult = () => {
   const num1 = Number(firstValue);
   const num2 = Number(secondValue);
   let resultValue = '';
-  endOperation = true;
+  str = '';
 
   switch (signMathematicalOperations) {
     case '+':
@@ -71,23 +62,50 @@ const calculateResult = () => {
   secondValue = '';
   firstValue = resultValue;
   result.innerHTML = resultValue;
-  console.log(firstValue, secondValue, 'v');
+  date = String(resultValue);
 };
 
+const test = value => {
+  let matchResult;
+  let results;
+  date = `${date}${value.trim()}`;
+  displayResult.innerHTML = `${date}`
+  matchResult = date.match(calculatorRegex);
+
+  if (matchResult !== null) {
+    results = matchResult[0].replace(replaceStr, '$1 $2 ').match(regex);
+    console.log(results);
+    firstValue = results[0].replace(/^0+/, "");
+    signMathematicalOperations = results[1];
+    secondValue = results[2].replace(/^0+/, "");
+    finalResults = true;
+  }
+};
 
 const onGetValue = value => {
-  if (mathematicalOperations.includes(value)) onOperations(value);
-  if (numbers.includes(value)) onSecondNumber(value);
-
-  onRenderOperation();
   if (mathematicalOperations.includes(value)) {
-    console.log(value);
-    // calculateResult();
-  }
-  if (value === '=') {
-    displayResult.innerHTML = `${onRenderOperation()} =`;
+    str = '';
+    result.innerHTML = '';
+  };
+
+  if (numbers.includes(value)) {
+    str += value;
+    result.innerHTML = str;
+  };
+
+  if (mathematicalOperations.includes(value)  && finalResults) {
     calculateResult();
-  }
+  };
+
+  if ((mathematicalOperations.includes(value) || numbers.includes(value))) {
+    test(value);
+  };
+ 
+  if (value === '=') {
+    finalResults = false;
+    displayResult.innerHTML = `${date.replace(/^0+/, "")} =`;
+    calculateResult();
+  };
 };
 
 const onInit = () => {
